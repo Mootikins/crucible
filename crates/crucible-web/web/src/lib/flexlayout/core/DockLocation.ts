ac923eff0a77e2c3b08c4b0e321dae858d6f92fa
Matthew Krohn <matthew.d.krohn@gmail.com>
2026-02-06 23:33:34 -0600

    feat(web): port FlexLayout core types and utilities
    - Port 8 core utility files from FlexLayout upstream
    - Rect: geometry class with 16 test cases
    - DockLocation: diagonal 5-zone detection with 14 tests
    - Orientation: HORZ/VERT enum with 4 tests
    - Attribute: configuration with 9 tests
    - AttributeDefinitions: schema + inheritance with 12 tests
    - DropInfo, I18nLabel, Types: supporting utilities
    - 61/61 tests passing
    - Zero React/DOM dependencies

---
+++ crates/crucible-web/web/src/lib/flexlayout/core/DockLocation.ts
@@ -0,0 +1,105 @@
+import { Orientation } from "./Orientation";
+import { Rect } from "./Rect";
+
+export class DockLocation {
+  static values = new Map<string, DockLocation>();
+  static TOP = new DockLocation("top", Orientation.VERT, 0);
+  static BOTTOM = new DockLocation("bottom", Orientation.VERT, 1);
+  static LEFT = new DockLocation("left", Orientation.HORZ, 0);
+  static RIGHT = new DockLocation("right", Orientation.HORZ, 1);
+  static CENTER = new DockLocation("center", Orientation.VERT, 0);
+
+  static getByName(name: string): DockLocation {
+    return DockLocation.values.get(name)!;
+  }
+
+  static getLocation(rect: Rect, x: number, y: number) {
+    x = (x - rect.x) / rect.width;
+    y = (y - rect.y) / rect.height;
+
+    if (x >= 0.25 && x < 0.75 && y >= 0.25 && y < 0.75) {
+      return DockLocation.CENTER;
+    }
+
+    const bl = y >= x;
+    const br = y >= 1 - x;
+
+    if (bl) {
+      return br ? DockLocation.BOTTOM : DockLocation.LEFT;
+    } else {
+      return br ? DockLocation.RIGHT : DockLocation.TOP;
+    }
+  }
+
+  name: string;
+  orientation: Orientation;
+  indexPlus: number;
+
+  constructor(_name: string, _orientation: Orientation, _indexPlus: number) {
+    this.name = _name;
+    this.orientation = _orientation;
+    this.indexPlus = _indexPlus;
+    DockLocation.values.set(this.name, this);
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getOrientation() {
+    return this.orientation;
+  }
+
+  getDockRect(r: Rect) {
+    if (this === DockLocation.TOP) {
+      return new Rect(r.x, r.y, r.width, r.height / 2);
+    } else if (this === DockLocation.BOTTOM) {
+      return new Rect(r.x, r.getBottom() - r.height / 2, r.width, r.height / 2);
+    }
+    if (this === DockLocation.LEFT) {
+      return new Rect(r.x, r.y, r.width / 2, r.height);
+    } else if (this === DockLocation.RIGHT) {
+      return new Rect(r.getRight() - r.width / 2, r.y, r.width / 2, r.height);
+    } else {
+      return r.clone();
+    }
+  }
+
+  split(rect: Rect, size: number) {
+    if (this === DockLocation.TOP) {
+      const r1 = new Rect(rect.x, rect.y, rect.width, size);
+      const r2 = new Rect(rect.x, rect.y + size, rect.width, rect.height - size);
+      return { start: r1, end: r2 };
+    } else if (this === DockLocation.LEFT) {
+      const r1 = new Rect(rect.x, rect.y, size, rect.height);
+      const r2 = new Rect(rect.x + size, rect.y, rect.width - size, rect.height);
+      return { start: r1, end: r2 };
+    }
+    if (this === DockLocation.RIGHT) {
+      const r1 = new Rect(rect.getRight() - size, rect.y, size, rect.height);
+      const r2 = new Rect(rect.x, rect.y, rect.width - size, rect.height);
+      return { start: r1, end: r2 };
+    } else {
+      const r1 = new Rect(rect.x, rect.getBottom() - size, rect.width, size);
+      const r2 = new Rect(rect.x, rect.y, rect.width, rect.height - size);
+      return { start: r1, end: r2 };
+    }
+  }
+
+  reflect() {
+    if (this === DockLocation.TOP) {
+      return DockLocation.BOTTOM;
+    } else if (this === DockLocation.LEFT) {
+      return DockLocation.RIGHT;
+    }
+    if (this === DockLocation.RIGHT) {
+      return DockLocation.LEFT;
+    } else {
+      return DockLocation.TOP;
+    }
+  }
+
+  toString() {
+    return "(DockLocation: name=" + this.name + ", orientation=" + this.orientation + ")";
+  }
+}
---
+++ crates/crucible-web/web/src/lib/flexlayout/core/Rect.ts
@@ -0,0 +1,126 @@
+import { Orientation } from "./Orientation";
+
+export interface IJsonRect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export class Rect {
+  static empty() {
+    return new Rect(0, 0, 0, 0);
+  }
+
+  static fromJson(json: IJsonRect): Rect {
+    return new Rect(json.x, json.y, json.width, json.height);
+  }
+
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  toJson() {
+    return { x: this.x, y: this.y, width: this.width, height: this.height };
+  }
+
+  snap(round: number) {
+    this.x = Math.round(this.x / round) * round;
+    this.y = Math.round(this.y / round) * round;
+    this.width = Math.round(this.width / round) * round;
+    this.height = Math.round(this.height / round) * round;
+  }
+
+  relativeTo(r: Rect | DOMRect) {
+    return new Rect(this.x - r.x, this.y - r.y, this.width, this.height);
+  }
+
+  clone() {
+    return new Rect(this.x, this.y, this.width, this.height);
+  }
+
+  equals(rect: Rect | null | undefined) {
+    return (
+      this.x === rect?.x &&
+      this.y === rect?.y &&
+      this.width === rect?.width &&
+      this.height === rect?.height
+    );
+  }
+
+  equalSize(rect: Rect | null | undefined) {
+    return this.width === rect?.width && this.height === rect?.height;
+  }
+
+  getBottom() {
+    return this.y + this.height;
+  }
+
+  getRight() {
+    return this.x + this.width;
+  }
+
+  get bottom() {
+    return this.y + this.height;
+  }
+
+  get right() {
+    return this.x + this.width;
+  }
+
+  getCenter() {
+    return { x: this.x + this.width / 2, y: this.y + this.height / 2 };
+  }
+
+  contains(x: number, y: number) {
+    if (this.x <= x && x <= this.getRight() && this.y <= y && y <= this.getBottom()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeInsets(insets: { top: number; left: number; bottom: number; right: number }) {
+    return new Rect(
+      this.x + insets.left,
+      this.y + insets.top,
+      Math.max(0, this.width - insets.left - insets.right),
+      Math.max(0, this.height - insets.top - insets.bottom)
+    );
+  }
+
+  centerInRect(outerRect: Rect) {
+    this.x = (outerRect.width - this.width) / 2;
+    this.y = (outerRect.height - this.height) / 2;
+  }
+
+  _getSize(orientation: Orientation) {
+    let prefSize = this.width;
+    if (orientation === Orientation.VERT) {
+      prefSize = this.height;
+    }
+    return prefSize;
+  }
+
+  toString() {
+    return (
+      "(Rect: x=" +
+      this.x +
+      ", y=" +
+      this.y +
+      ", width=" +
+      this.width +
+      ", height=" +
+      this.height +
+      ")"
+    );
+  }
+}
