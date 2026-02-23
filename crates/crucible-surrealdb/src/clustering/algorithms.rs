0501b7b1fb7da60a5d245b77660811a12e8f1da0
Matthew Krohn <matthew.d.krohn@gmail.com>
2026-02-23 16:47:09 -0600

    feat!: delete crucible-surrealdb (17K LOC)

--- crates/crucible-surrealdb/src/clustering/algorithms.rs
+++
@@ -1,7 +0,0 @@
-//! Built-in clustering algorithm implementations
-
-pub use self::heuristic::{HeuristicAlgorithmFactory, HeuristicClusteringAlgorithm};
-pub use self::kmeans::{KMeansAlgorithmFactory, KMeansClusteringAlgorithm};
-
-pub mod heuristic;
-pub mod kmeans;
